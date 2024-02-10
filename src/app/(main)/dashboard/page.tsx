2abc88892bfc1b165ee99239592ac2b2f0f94b20
Abdul Samad Gomda <ag7766@nyu.edu>
2024-02-10 16:32:50 +0400

    Add shadcn-ui support and anticipated routes

---
+++ src/app/(main)/dashboard/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const DashboardPage = () => {
+	return (
+		<div
+			className="bg-background h-screen
+            w-screen
+            flex
+            justify-center
+            items-center
+            gap-4 dark:text-white"
+		>
+			DashboardPage
+		</div>
+	);
+};
+
+export default DashboardPage;
---
+++ src/app/(main)/upload/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const UploadPage = () => {
+	return (
+		<div
+			className="bg-background h-screen
+            w-screen
+            flex
+            justify-center
+            items-center
+            gap-4 dark:text-white"
+		>
+			UploadPage
+		</div>
+	);
+};
+
+export default UploadPage;
